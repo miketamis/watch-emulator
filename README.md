71e17c843979ab56a40a0daf1d5a2d6de2ccbc96
Mike Tamis <mike@navicomdynamics.com>
2015-08-10 18:33:19 +1200

    removed watch app as this is getting its own repo

--- README.md
+++
@@ -1,5 +0,0 @@
-# Situps-app
-Situps app for COSC345
-
-Link to assignment 1:
-http://miketamis.github.io/Situps-app/asignment1.html
--- demoApp/scripts.js
+++
@@ -1,6 +0,0 @@
-/* @flow */
-
-window.addEventListener("message", function(message) {
-  document.getElementsByTagName("span")[0].innerHTML = message.data;
-
-});
